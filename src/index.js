2c41d55cb4e7c2e1bc9802fb112e7a710965a479
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-26 19:36:32 -0400

    ability to add dog imgs in the image container

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-console.log('%c HI', 'color: firebrick')
 
 /* 
 1. on page load, fetches the images using the url above ⬆️
@@ -6,10 +5,8 @@ console.log('%c HI', 'color: firebrick')
 3. adds image elements to the DOM for each 🤔 image in the array
 */
 
-document.addEventListener("DOMContentLoaded", fetchData)
 
 // Global Variables
-
 const dogImageCont = document.getElementById("dog-image-container")
 console.log(dogImageCont)
 
@@ -18,6 +15,19 @@ console.log(dogImageCont)
 function fetchData(){
 fetch( "https://dog.ceo/api/breeds/image/random/4")
 .then((response) => response.json())
-.then((data) => console.log(data) )
-.catch((error) => console.log('error!:', error))
+.then((data) => addDom(data.message))
 }
+fetchData()
+
+
+function addDom(data){
+    for (let elements of data){
+     const imageEle = document.createElement('img');
+     imageEle.setAttribute("src", elements)
+     imageEle.setAttribute('height', '250px' )
+     imageEle.setAttribute('width', '250px')
+     imageEle.innerHTML = elements
+     dogImageCont.appendChild(imageEle)
+    }
+}
+
