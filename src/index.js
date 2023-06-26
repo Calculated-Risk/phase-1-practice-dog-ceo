0eb4c50bbc49c445b36148ee00e4b5feb64eabd3
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-25 20:52:53 -0400

    starting foundation of lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+/* 
+1. on page load, fetches the images using the url above ⬆️
+2. parses the response as JSON
+3. adds image elements to the DOM for each 🤔 image in the array
+*/
+
+document.addEventListener("DOMContentLoaded", fetchData)
+
+// Global Variables
+
+const dogImageCont = document.getElementById("dog-image-container")
+console.log(dogImageCont)
+
+
+
+function fetchData(){
+fetch( "https://dog.ceo/api/breeds/image/random/4")
+.then((response) => response.json())
+.then((data) => console.log(data) )
+.catch((error) => console.log('error!:', error))
+}
