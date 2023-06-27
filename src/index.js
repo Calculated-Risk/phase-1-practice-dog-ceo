5bb9c9d41dea2ec316bd2598c29fff5880a71f73
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-26 20:07:54 -0400

    updated unordered list with dog types

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 
 /* 
-1. on page load, fetches the images using the url above ⬆️
+1. on page load, fetches the images using the url 
 2. parses the response as JSON
 3. adds image elements to the DOM for each 🤔 image in the array
 */
 
 
-// Global Variables
-const dogImageCont = document.getElementById("dog-image-container")
-console.log(dogImageCont)
 
 
+//2. parses the response as JSON
 
 function fetchData(){
 fetch( "https://dog.ceo/api/breeds/image/random/4")
@@ -20,14 +18,41 @@ fetch( "https://dog.ceo/api/breeds/image/random/4")
 fetchData()
 
 
+//3. adds image elements to the DOM for each 🤔 image in the array
+
 function addDom(data){
     for (let elements of data){
+     const dogImageCont = document.getElementById("dog-image-container")   
      const imageEle = document.createElement('img');
      imageEle.setAttribute("src", elements)
      imageEle.setAttribute('height', '250px' )
-     imageEle.setAttribute('width', '250px')
+     imageEle.setAttribute('width', '260px')
      imageEle.innerHTML = elements
      dogImageCont.appendChild(imageEle)
     }
 }
 
+
+/*    
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+After the first challenge is completed, add JavaScript that:
+
+on page load, fetches all the dog breeds using the url above ⬆️
+adds the breeds to the page in the <ul> provided in index.html
+*/
+
+function fetchDataAll(){
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then((response) => response.json())
+    .then((data) => addList(data.message))
+    }
+fetchDataAll()
+
+function addList(dogs){
+ for (let dog in dogs){
+    const list = document.getElementById("dog-breeds");
+    const listItem = document.createElement("li");
+    listItem.innerHTML = dog
+    list.appendChild(listItem)
+ }
+}
